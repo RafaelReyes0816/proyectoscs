85b75533eb4b01809359cf43f300ffcd355eef75
RafaelReyes0816 <tj.juan.reyes.b@upds.net.bo>
2025-05-16 17:33:06 -0400

    Agregar Tarea-01 como submódulo

--- clases/obj/Debug/net9.0/clases.AssemblyInfo.cs
+++ clases/obj/Debug/net9.0/clases.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("clases")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9e302b0972febd9c8ba6332ce4dbaf836349a171")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1a1f5b27159ba79767142acfbc77ca7bfb0b60db")]
 [assembly: System.Reflection.AssemblyProductAttribute("clases")]
 [assembly: System.Reflection.AssemblyTitleAttribute("clases")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
